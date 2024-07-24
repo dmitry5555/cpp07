5caed02e6d24237469e654dfa002c76dec84b995
Dmitrii Larionov <mikrolux@gmail.com>
2024-07-24 20:49:37 +0700

    up

--- 00/src/main.cpp
+++ 00/src/main.cpp
@@ -1,11 +1,28 @@
 #include "whatever.hpp"
 
+template <typename T>
+void swap(T& a, T& b) {
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+T min(const T& a, const T& b) {
+	return (a < b) ? a : b;
+}
+
+template <typename T>
+T max(const T& a, const T& b) {
+	return (a > b) ? a : b;
+}
 
 int main(int argc, char **argv) {
-	int a = 1;
-	int b = 2;
+	char a = 1;
+	char b = 1;
+	// char b = 'a';
 
-	swap(a, b);
+	::swap(a, b); 
 	std::cout << "a is now " << a << " and b is now " << b << std::endl;
 	return 0;
 }
